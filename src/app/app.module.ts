a13236c99712c75b29e34444f77da3c4c2c751d7
dnpham <dongpham@kms-technology.com>
2019-05-20 11:27:09 +0700

    Initialise angular-universal  project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+
+import {Routes,RouterModule} from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+const routes: Routes = [
+  {
+    path: '',
+    component:HomePageComponent
+  },
+  {
+    path: 'items',
+    component: ListComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
